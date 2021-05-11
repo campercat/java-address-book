0099839b27cc0d616af6441b8e3c1d2cfdc33ee1
Suey Chen <suey.chen.16@gmail.com>
2021-05-10 21:47:04 -0400

    complete non-core stories

--- src/main/java/AddressBookRunner.java
+++ src/main/java/AddressBookRunner.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -10,14 +14,20 @@ import java.util.Set;
 public class AddressBookRunner {
   public static final String ADD_CONTACT = "Add a contact";
   public static final String LOOKUP_CONTACT = "Look up a contact";
-  public static final String LIST_ALL_CONTACTS = "List All Contacts";
+  public static final String LIST_ALL_CONTACTS = "List all contacts";
+  public static final String CLEAR_ALL_CONTACTS = "Clear all contacts";
+  public static final String SAVE_CONTACTS = "Save contacts";
+  public static final String LOAD_ADDRESS_BOOK = "Load address book";
   public static final String QUIT_TEXT = "Quit";
 
   public static void main(String[] args) {
-    List<String> options = new ArrayList<String>();
+    List<String> options = new ArrayList<>();
     options.add(ADD_CONTACT);
     options.add(LOOKUP_CONTACT);
     options.add(LIST_ALL_CONTACTS);
+    options.add(CLEAR_ALL_CONTACTS);
+    options.add(SAVE_CONTACTS);
+    options.add(LOAD_ADDRESS_BOOK);
     options.add(QUIT_TEXT);
 
     Scanner inputScanner = new Scanner(System.in);
@@ -31,6 +41,7 @@ public class AddressBookRunner {
         System.out.println(menuIndex + ". " + option);
         menuIndex++;
       }
+      System.out.println("Total number of contacts: " + addressBook.size());
 
       System.out.println("\nWhat would you like to do? Select a number.");
       try {
@@ -42,13 +53,26 @@ public class AddressBookRunner {
           inputScanner.nextLine();
           String name = "";
           String email = "";
+          String overwrite;
           while(name.isBlank()) {
             System.out.println("Enter the contact's name:");
             name = inputScanner.nextLine();
-          }
-          while(email.isBlank()) {
-            System.out.println("Enter the contact's email:");
-            email = inputScanner.nextLine();
+            if(addressBook.containsKey(name)) {
+              System.out.println("Contact already exists! Confirm to overwrite y/n:");
+              overwrite = inputScanner.nextLine();
+              if(overwrite.equals("y")) {
+                System.out.println("Inside if");
+                while(email.isBlank()) {
+                  System.out.println("Enter the contact's email:");
+                  email = inputScanner.nextLine();
+                }
+              }
+            } else {
+              while(email.isBlank()) {
+                System.out.println("Enter the contact's email:");
+                email = inputScanner.nextLine();
+              }
+            }
           }
           addressBook.put(name, email);
         } else if (menuChoice == LOOKUP_CONTACT) {
@@ -78,8 +102,55 @@ public class AddressBookRunner {
           }
           System.out.println("Total number of contacts: " + counter);
           System.out.println(builder);
+        } else if(menuChoice == CLEAR_ALL_CONTACTS) {
+          inputScanner.nextLine();
+          String overwrite;
+          System.out.println("Confirm to clear all existing contacts y/n:");
+          overwrite = inputScanner.nextLine();
+          if(overwrite.equals("y")) {
+            addressBook.clear();
+          }
+        } else if(menuChoice == SAVE_CONTACTS) {
+          inputScanner.nextLine();
+          String fileName = "";
+          System.out.println("Enter a name for your file:");
+          while(fileName.length() <= 2) {
+            fileName = inputScanner.nextLine();
+            System.out.println("File name must be longer than 2 characters. Please try again:");
+          }
+          File newFile = new File("./" + fileName + ".csv");
+          FileWriter writer = new FileWriter(newFile);
+          Set<String> names = addressBook.keySet();
+          int counter = 0;
+          StringBuilder builder = new StringBuilder();
+          for(String name : names) {
+            counter++;
+            writer.write(name + ", " + addressBook.get(name));
+            if(counter < addressBook.size()) {
+              writer.write("\n");
+            }
+          }
+          writer.close();
+        } else if(menuChoice == LOAD_ADDRESS_BOOK) {
+          inputScanner.nextLine();
+          String fileName;
+          System.out.println("Enter the name of the address book file:");
+          fileName = inputScanner.nextLine();
+          File existingFile = new File("./" + fileName + ".csv");
+          if(existingFile.exists()) {
+            addressBook.clear();
+            Scanner fileScanner = new Scanner(existingFile);
+            fileScanner.useDelimiter("[,\n]");
+            while(fileScanner.hasNextLine()) {
+              String name = fileScanner.next();
+              String email = fileScanner.next().trim();
+              addressBook.put(name, email);
+            }
+            fileScanner.close();
+          }
         }
-      } catch(IndexOutOfBoundsException err) {
+      } catch(IndexOutOfBoundsException | IOException err) {
+        inputScanner.close();
         System.out.println("Please make a valid selection!");
       }
     }
